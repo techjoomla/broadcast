30b7d3c66098c263a68419411bdb908860d98e23
Vaishali Khandagle <vaishali_k@tekdi.net>
2011-10-10 09:35:27 +0000

    api layout after importing contats from any api
    git-svn-id: svn+ssh://172.132.45.1/broadcast/trunk@164 38cc452e-e193-40ed-822d-ea8e43af1bb2

--- techjoomlaAPI/plug_techjoomlaAPI_linkedin/plug_techjoomlaAPI_linkedin.php
+++ techjoomlaAPI/plug_techjoomlaAPI_linkedin/plug_techjoomlaAPI_linkedin.php
@@ -49,7 +49,7 @@ class plgTechjoomlaAPIplug_techjoomlaAPI_linkedin extends JPlugin
 		$plug=array(); 
    	$plug['name']="Linkedin";
   	//check if keys are set
-		if($this->API_CONFIG['appKey']=='' || $this->API_CONFIG['appSecret']=='')// || !in_array($this->_name,$config)) #TODO add condition to check config
+		if($this->API_CONFIG['appKey']=='' || $this->API_CONFIG['appSecret']=='' || !in_array($this->_name,$config)) #TODO add condition to check config
 		{	
 			$plug['error_message']=true;		
 			return $plug;
