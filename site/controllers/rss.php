98b9820f066d6eb8452734af794961b84db94aa8
Sagar Chinchawde <sagar_c@tekdi.net>
2012-05-28 07:12:43 +0000

    Feature #11865 Add configuration for limit for rss feed per user  Ticket ID #4774
    git-svn-id: svn+ssh://172.132.45.1/broadcast/trunk@271 38cc452e-e193-40ed-822d-ea8e43af1bb2

--- site/controllers/rss.php
+++ site/controllers/rss.php
@@ -45,7 +45,7 @@ class BroadcastControllerrss extends JController
 			$database->setQuery($fbquery);
 			$this->links = $database->loadObjectlist();
 			$links=explode('|',$this->links[0]->broadcast_rss_url);
-			if(!empty($broadcast_config['rss_limit_per_user'])
+			if(!empty($broadcast_config['rss_limit_per_user']))
 			$rss_limit_per_user=$broadcast_config['rss_limit_per_user'];
 			else
 			$rss_limit_per_user=5;
